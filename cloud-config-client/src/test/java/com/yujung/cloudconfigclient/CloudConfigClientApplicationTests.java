e23850bd457295f775d2372a6e1f0a1a6131d1bb
yujung-Jang <yjung.dev@gmail.com>
2023-06-07 23:41:22 +0900

    :sparkles: Feat: Spring Cloud Config Client 구성

---
+++ cloud-config-client/src/test/java/com/yujung/cloudconfigclient/CloudConfigClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yujung.cloudconfigclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudConfigClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
