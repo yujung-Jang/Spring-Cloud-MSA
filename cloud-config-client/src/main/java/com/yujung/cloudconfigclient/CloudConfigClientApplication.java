1bab1871aec10d6b981d05380dcb7c472c331305
yujung-Jang <yjung.dev@gmail.com>
2023-06-07 23:47:58 +0900

    :white_check_mark: Feat: Spring Cloud Config 동작 테스트

--- cloud-config-client/src/main/java/com/yujung/cloudconfigclient/CloudConfigClientApplication.java
+++ cloud-config-client/src/main/java/com/yujung/cloudconfigclient/CloudConfigClientApplication.java
@@ -2,8 +2,10 @@ package com.yujung.cloudconfigclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(MyConfig.class)
 public class CloudConfigClientApplication {
 
 	public static void main(String[] args) {
---
+++ cloud-config-client/src/main/java/com/yujung/cloudconfigclient/MyConfig.java
@@ -0,0 +1,18 @@
+package com.yujung.cloudconfigclient;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+
+@Getter
+@Setter
+@ToString
+@RefreshScope
+@ConfigurationProperties("com.yujung.cloud-client")
+public class MyConfig {
+
+    private String profile;
+    private String region;
+}
