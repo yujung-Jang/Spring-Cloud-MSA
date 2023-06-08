321348e4a6583afa988003f81e2e21ea4edc60ad
yujung-Jang <yjung.dev@gmail.com>
2023-06-08 18:04:36 +0900

    :recycle: Refactor: 설정 파일 사소한 구성 변경

--- cloud-config-client/src/main/java/com/yujung/cloudconfigclient/MyConfig.java
+++ cloud-config-client/src/main/java/com/yujung/cloudconfigclient/MyConfig.java
@@ -4,12 +4,11 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @Getter
 @Setter
 @ToString
-@RefreshScope
+//@RefreshScope
 @ConfigurationProperties("com.yujung.client")
 public class MyConfig {
 
